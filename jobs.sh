b16e3fa8622c89dd8832de1ef2e637e223fd5508
T. Tran <t-tran@users.noreply.github.com>
2022-12-08 11:54:51 +0700

    Look at all conversations in the past to run backup

--- jobs.sh
+++ jobs.sh
@@ -7,23 +7,25 @@ trap "for i in \$(jobs -p); do kill \$i; done; exit" SIGINT SIGHUP
 
 echo "Job manager - start : $MAX_THREADS jobs at a time!"
 
-for t in ims mpims channels; do
-  IDS=$(cat meta/$team_name/boot.json | jq -r '.'$t'[].id')
-  for i in $IDS; do
-    # check if we can skip archived channels since they should not have any new messages
-    if [[ "X$t" == "Xchannels" ]]; then
-      if [[ $SKIP_ARCHIVED_CHANNELS -gt 0 ]]; then
-        is_archived=$(cat meta/$team_name/boot.json | jq -r $'.channels[]|select(.id=="'$i$'")|.is_archived')
-      fi
+CONVERSATIONS=$(cat meta/$team_name/boot.json | jq -r '.last_read|keys[]')
+for i in $CONVERSATIONS; do
+  t=none
+  [[ $i == "D"* ]] && t=ims
+  [[ $i == "G"* ]] && t=mpims
+  [[ $i == "C"* ]] && t=channels
+  # check if we can skip archived channels since they should not have any new messages
+  if [[ "X$t" == "Xchannels" ]]; then
+    if [[ $SKIP_ARCHIVED_CHANNELS -gt 0 ]]; then
+      is_archived=$(cat meta/$team_name/boot.json | jq -r $'.channels[]|select(.id=="'$i$'")|.is_archived')
     fi
-    ./conversation.sh $t $i &
-    while [[ true ]]; do
-      RUNNING=$(jobs | grep -c 'Running')
-      if [[ $RUNNING -lt $MAX_THREADS ]]; then
-        break
-      fi
-      sleep 1
-    done
+  fi
+  ./conversation.sh $t $i &
+  while [[ true ]]; do
+    RUNNING=$(jobs | grep -c 'Running')
+    if [[ $RUNNING -lt $MAX_THREADS ]]; then
+      break
+    fi
+    sleep 1
   done
 done
 
